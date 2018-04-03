e061b4959c1cf8fed4d02d2ea8bc560c6854dc4c
u211-02 <u211-02@public.mpei.local>
2018-04-03 11:57:51 +0300

    2 комми

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <sstream>
 #include <cassert>
 using namespace std;
@@ -32,13 +32,41 @@ void test_temperature_input() {
 double convert(const Temperature& Temp,char scale_to )
 {
 	double Kelvins;
+	if (scale_to==Temp.scale) return Temp.temp;
 	switch (Temp.scale) {
-	case 'F':Kelvins+=((9.0/5)*(Temp.temp-273)+32);
+	case 'F':Kelvins=(5*(Temp.temp-32))/9+273.15;
 		break;
 	case 'C':Kelvins+=-273.15;
 		break;
 	}
+	switch (scale_to) {
+	case 'C':
+	case 'c':
+		return Kelvins - 273.15;
+	case 'K':
+	case 'k':
+		return Kelvins;
+	case 'F':
+	case 'f':
+		return Kelvins-((9.0/5)*(Temp.temp-273.15)+32);
+	}}
+	bool operator < (const Temperature & lhs,const Temperature & rhs) {
+		return convert(lhs, 'K') < convert(rhs, 'K');
+	}
+	Temperature operator-(const Temperature & lhs, const Temperature &rhs)
+	{
+		Temperature ret_temp;
+		ret_temp.temp=convert(lhs,'K')-convert(rhs,'K');
+		ret_temp.scale='K';
+		return ret_temp;
 }
+	Temperature operator/(const Temperature & lhs, const Temperature & rhs)
+	{
+		Temperature ret_temp;
+		ret_temp.temp=convert(lhs,'K')/convert(rhs,'K');
+		ret_temp.scale='K';
+		return ret_temp;
+	}
 	int main() {
 		test_temperature_input();
-}
+	}
